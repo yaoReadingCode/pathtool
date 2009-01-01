3f4306deabeb7291f93072192421c3facc16f7e1
noah.gift <noah.gift@01a06be0-f351-0410-99ed-8d8d91030462>
2009-01-01 01:25:22 +0000

    adding working FileOb

--- pathtool/tests/test_genwalk.py
+++ pathtool/tests/test_genwalk.py
@@ -9,19 +9,29 @@ except ImportError:
     from pathtool import genwalk
 
 class TestFileOb(unittest.TestCase):
-    
+
     def setUp(self):
         pass
 
     def test_init(self):
-        """Test General Package Import.  
-        
+        """Test General Package Import.
+
         This just ensures package is in path, so tests work"""
-        
+
         try:
             genwalk.FileOb()
         except:
             self.fail()
+    
+class TestWalk(unittest.TestCase):
+
+    def setUp(self):
+
+        self.path = "tests/test_data"
+
+    def test_mkfileob(self):
+
+        walk = genwalk.Walk(self.path)
 
 if __name__ == "__main__":
-    unittest.main() 
+    unittest.main()
